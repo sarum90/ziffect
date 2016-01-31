ddb55a00fbfce1bbdb7b5b5ed674fac2ff5e49b7
Marcus Henry Ewert <user@marcushenryewert.com>
2016-01-31 08:47:32 -0800

    remove skip tracebacks

--- ziffect/doc/__init__.py
+++ ziffect/doc/__init__.py
@@ -14,18 +14,33 @@ def cleanup(lines):
             hashes[addr] = '0x7fff%04d' % len(hashes.keys())
         return hashes[addr]
 
-    def clean_filename(match):
-        file = match.group(1)
-        sp = 'site-packages'
-        if sp in file:
-            newfile = file.split(sp)[-1][1:]
-        else:
-            newfile = '<interactive-shell>'
-        return 'File "%s"' % newfile
+    def clean_filename(line):
+        regex = r'File "([^"]*)"'
 
+        def _clean_filename(match):
+            file = match.group(1)
+            sp = 'site-packages'
+            if sp in file:
+                newfile = file.split(sp)[-1][1:]
+            else:
+                newfile = '<interactive-shell>'
+            return 'File "%s"' % newfile
+
+        line = re.sub(regex, _clean_filename, line)
+
+        skip = 0
+        if 'File "six' in line:
+            skip = 2
+        return line, skip
+
+    skip = 0
     for line in lines.splitlines():
         line = re.sub(r'0x[0-9a-f]*', get_print_addr, line)
-        line = re.sub(r'File "([^"]*)"', clean_filename, line)
+        if not skip:
+            line, skip = clean_filename(line)
+        if skip > 0:
+            skip -= 1
+            continue
         result.append(line)
 
     return '\n'.join(result)
